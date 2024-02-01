e6f1a5c4eb88a61a0c8b832ef61d22d73ed9358c
Yamini855 <vermayamini855@gmail.com>
2024-02-01 20:37:19 +0530

    New Commit

--- App1.java
+++ App1.java
@@ -3,6 +3,7 @@ public class App1{
     public static void main(String[] args){
         System.out.println("Java App Pulled & Build by Jenkins");
         System.out.println("Hello Java and Jenkins");
+        System.out.println("Hello World");
 
     }
 }
